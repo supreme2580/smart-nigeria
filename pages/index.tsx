03c12a324f0aafab8909acb651e7af6907e1c3c1
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-28 16:25:06 +0100

    suprememods

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,17 +14,17 @@ export default function Home() {
             <div>
               <Image src={"/nigeria.png"} width={75} height={75} alt="" />
             </div>
-            <h1 className="text-6xl font-semibold">SMART NIGERIA</h1>
+            <h1 className="text-3xl sm:text-6xl font-semibold">SMART NIGERIA</h1>
           </div>
           <div className="w-full text-center">
             <p className="font-semibold italic">
               "Incentivizing change in africa"
             </p>
           </div>
-          <div className="w-full space-x-5 flex justify-center">
-            <button className="py-2.5 px-10 border-2 border-black rounded-[50px] font-semibold">Login</button>
-            <button className="py-2.5 px-10 border-2 border-black rounded-[50px] font-semibold">Sign up</button>
-            <button className="py-2.5 px-10 border-2 border-black rounded-[50px] font-semibold">Support us</button>
+          <div className="w-full sm:space-x-5 flex flex-col sm:flex-row space-y-2.5 sm:space-y-0 justify-center">
+            <button className="py-2.5 px-10 border-2 border-black rounded-[50px] font-semibold hover:bg-black hover:text-white">Login</button>
+            <button className="py-2.5 px-10 border-2 border-black rounded-[50px] font-semibold hover:bg-black hover:text-white">Sign up</button>
+            <button className="py-2.5 px-10 border-2 border-black rounded-[50px] font-semibold hover:bg-black hover:text-white">Support us</button>
           </div>
         </div>
       </main>
