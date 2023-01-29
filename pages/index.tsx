bf2ea8924bae4ad676c8ab2e133b9c9297ed4d35
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-29 08:11:19 +0100

    suprememods

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,50 @@ import Image from "next/image";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import { useSigner } from "wagmi";
-import { useRouter } from "next/router";
+import { useRouter } from "next/router"
+import lookup from "coordinate_to_country"
+import { useState } from "react";
 
 export default function Home() {
 
   const { openConnectModal } = useConnectModal()
   const { data: signer } = useSigner()
   const router = useRouter()
+  const [error, setError] = useState(false)
+  const [iso, setIso] = useState(null)
+
+  async function showLocation(position: any) {
+    var latitude = position.coords.latitude
+    var longitude = position.coords.longitude
+    const result = lookup(longitude, latitude)
+    if (result != "NGA") {
+      setError(true)
+      setIso(result)
+    }
+    else {
+      router.push("/main")
+    }
+  }
+
+  function errorHandler(err: any) {
+      if(err.code == 1) {
+        alert("Error: Access is denied!");
+      } else if( err.code == 2) {
+        alert("Error: Position is unavailable!");
+      }
+  }
+
+  async function getLocation() {
+
+      if(navigator.geolocation) {
+        
+        // timeout at 60000 milliseconds (60 seconds)
+        var options = {timeout:60000};
+        await navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
+      } else {
+        alert("Sorry, browser does not support geolocation!");
+      }
+  }
   
   return(
     <div>
@@ -30,16 +67,21 @@ export default function Home() {
               {'"'}Incentivizing change in Nigeria{'"'}
             </p>
           </div>
+          {
+            iso && <div>
+            <p className="text-red-700 font-semibold">Unsupported region: {iso}</p>
+          </div>
+          }
           <div className="w-full sm:space-x-5 flex flex-col sm:flex-row space-y-2.5 sm:space-y-0 justify-center">
             {
               signer ?
               <button className="py-2.5 px-10 border-2 border-black rounded-[50px] font-semibold hover:bg-black hover:text-white
-                flex items-center space-x-1 group" onClick={() => router.push("/main")}>
+                flex items-center justify-center space-x-1 group" onClick={async() => await getLocation()}>
                 <p>Enter</p>
                 <ArrowRightIcon className="w-5 h-5 text-black group-hover:text-white" />
               </button> :
               <button className="py-2.5 px-10 border-2 border-black rounded-[50px] font-semibold hover:bg-black hover:text-white
-                flex justify-center items-center space-x-5 group" onClick={openConnectModal}>Connect Wallet</button>
+                flex items-center justify-center space-x-5 group" onClick={openConnectModal}>Connect Wallet</button>
             }
             <button className="py-2.5 px-10 border-2 border-black rounded-[50px] font-semibold hover:bg-black hover:text-white">Support us</button>
           </div>
